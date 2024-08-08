164aa6643fda7665eba3cfe741825172b6d1d3a4
vilarj <lamaravillasan@outlook.com>
2024-08-08 15:37:25 -0400

    Gramatica | slogan component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SloganComponent } from './slogan/slogan.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, MenuComponent],
+  imports: [RouterOutlet, NavbarComponent, MenuComponent, SloganComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/slogan/slogan.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'slogan',
+  standalone: true,
+  imports: [],
+  templateUrl: './slogan.component.html',
+  styleUrl: './slogan.component.css',
+})
+export class SloganComponent {}
