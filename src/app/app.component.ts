7fc92ba4c9f8eb197ac6fd4be5244be7264d23a2
vilarj <lamaravillasan@outlook.com>
2024-08-09 10:01:57 -0400

    Footer component + icon | styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 import { MenuComponent } from './menu/menu.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SloganComponent } from './slogan/slogan.component';
 
 @Component({
-  selector: 'app-root',
+  selector: 'root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, MenuComponent, SloganComponent],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    MenuComponent,
+    SloganComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
